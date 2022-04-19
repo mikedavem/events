c8579738ed611eaf018b3802a8f84bcc9cc421a2
mikedavem <mikedavem1@hotmail.com>
2022-04-19 22:15:20 +0200

    init scripts

--- post_install.ps1
+++ post_install.ps1
@@ -16,7 +16,7 @@ Try {
         If ($i -eq 0){ $database = 'master'} Else { $database = 'DBA' }
 
         #Write-Host "Executing file $file - DB $database"
-        Invoke-DbaQuery -SqlInstance $SqlInstance -Database $database -File $file -SqlCredential $SqlCredential -EnableException 
+        Invoke-DbaQuery -SqlInstance "localhost\$SqlInstance" -Database $database -File $file -SqlCredential $SqlCredential -EnableException 
 
         $i++
     }
